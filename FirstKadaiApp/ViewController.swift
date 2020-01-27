ccc60f4b45021f500fa783f111358a08787f79db
fu <amcome450@gmail.com>
2020-01-27 09:56:13 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by jumu on 2020/01/27.
+//  Copyright © 2020 jumu.furukawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
